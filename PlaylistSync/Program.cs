88315409a34c6ac39bd97a863a25ccc3c0ee61b8
mhtsbt <matthias.hutsebaut@gmail.com>
2018-09-26 19:52:16 +0200

    Add project files.

---
+++ PlaylistSync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlaylistSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
