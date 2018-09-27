64ed4e2ec1a5dfa1829014451cd9fb160843e1ea
mhtsbt <matthias.hutsebaut@gmail.com>
2018-09-27 22:05:27 +0200

    added m3u

--- PlaylistSync/Program.cs
+++ PlaylistSync/Program.cs
@@ -1,6 +1,10 @@
 ﻿using ITunesLibraryParser;
 using Microsoft.Extensions.DependencyInjection;
+using PlaylistsNET.Content;
+using PlaylistsNET.Models;
 using System;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -8,7 +12,7 @@ namespace PlaylistSync
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
 
             string iTunesXml = @"C:\Users\matth\Music\iTunes\iTunes Music Library.xml";
@@ -22,15 +26,38 @@ namespace PlaylistSync
             // returns all albums in the iTunes Library
 
             var playlists = library.Playlists;
+
+            var favTracks = playlists.FirstOrDefault(x => x.Name == "Favorites").Tracks;
+
             // returns all playlists in the iTunes Library
 
-            var client = new HttpClient();
-       
 
             Console.WriteLine("Hello World!");
 
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddLogging();
+
+
+            M3uPlaylist playlist = new M3uPlaylist();
+            playlist.IsExtended = true;
+
+            foreach (var track in favTracks)
+            {
+
+                playlist.PlaylistEntries.Add(new M3uPlaylistEntry()
+                {
+                    Album = track.Album,
+                    AlbumArtist = track.Artist,                    
+                    Path = track.Location,
+                    Title = track.Name
+                });
+
+            }
+
+            M3uContent content = new M3uContent();
+            string text = content.ToText(playlist);
+
+            File.WriteAllText("test.m3u", text);
 
         }
 
